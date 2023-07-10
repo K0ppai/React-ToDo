82fc84895d776886c370548eda6d14d7074ec3f8
koppai <psthu.koppai@gmail.com>
2023-07-10 19:33:43 +0630

    Create components folder and add required files inside

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,67 @@
+import styles from '@/styles/TodoItem.module.css';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const TodoItem = ({ itemProp, handleChange, delTodo, setUpdate }) => {
+  const [updateInput, setUpdateInput] = useState(itemProp.title);
+  const [editing, setEditing] = useState(false);
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+
+  let viewMode = {};
+  let editMode = {};
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+
+  const handleEditing = () => {
+    setEditing(true);
+  };
+  
+  const handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      setUpdate(updateInput, itemProp.id);
+      setEditing(false);
+    }
+  };
+
+  return (
+    <li className={styles.item}>
+      <div className={styles.content} style={viewMode}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        <button onClick={handleEditing}>Edit</button>
+        <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+        <span style={itemProp.completed ? completedStyle : null}>
+          {updateInput}
+        </span>
+      </div>
+      <input
+      type="text"
+      value={updateInput}
+      className={styles.textInput}
+      style={editMode}
+      onChange={(e) => setUpdateInput(e.target.value)}
+      onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  );
+};
+
+TodoItem.propTypes = {
+  itemProp: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+  setUpdate: PropTypes.func.isRequired,
+};
+
+export default TodoItem;
